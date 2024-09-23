65d8433563873551279ebce61ba7536fd04eae91
André L Reis <gato5andre26@gmail.com>
2024-09-23 22:08:56 +0100

    feat: base project

---
+++ lms/target/generated-sources/annotations/pt/psoft/g1/psoftg1/lendingmanagement/api/FineViewMapperImpl.java
@@ -0,0 +1,13 @@
+package pt.psoft.g1.psoftg1.lendingmanagement.api;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-09-17T19:55:15+0100",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.4 (Amazon.com Inc.)"
+)
+@Component
+public class FineViewMapperImpl extends FineViewMapper {
+}
