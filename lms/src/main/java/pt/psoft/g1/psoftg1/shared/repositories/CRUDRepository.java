f766a2205d1d18c014eca556627a5ae2fa5e15f3
André L Reis <gato5andre26@gmail.com>
2024-10-07 22:58:58 +0100

    feat: mongodb support + repo CRUD interface

---
+++ lms/src/main/java/pt/psoft/g1/psoftg1/shared/repositories/CRUDRepository.java
@@ -0,0 +1,10 @@
+package pt.psoft.g1.psoftg1.shared.repositories;
+
+import java.util.List;
+
+public interface CRUDRepository<T, ID> {
+    T save(T entity);
+    void delete(ID id);
+    List<T> findAll();
+    T findById(ID id);
+}
