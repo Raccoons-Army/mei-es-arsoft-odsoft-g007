5bccc62e5fcd1911ab1a5a3cdabcc20a5a5f683f
André L Reis <gato5andre26@gmail.com>
2024-10-10 01:43:11 +0100

    feat: Generic mapper + example

--- lms/src/main/java/pt/psoft/g1/psoftg1/bookmanagement/mapper/BookMapper.java
+++ lms/src/main/java/pt/psoft/g1/psoftg1/bookmanagement/mapper/BookMapper.java
@@ -1,4 +1,29 @@
 package pt.psoft.g1.psoftg1.bookmanagement.mapper;
 
-public class BookMapper {
+import pt.psoft.g1.psoftg1.bookmanagement.dbSchema.JpaBookDTO;
+import pt.psoft.g1.psoftg1.bookmanagement.dbSchema.MongoBookDTO;
+import pt.psoft.g1.psoftg1.bookmanagement.model.Book;
+import pt.psoft.g1.psoftg1.shared.mapper.Mapper;
+
+public class BookMapper implements Mapper<JpaBookDTO, MongoBookDTO, Book> {
+
+    @Override
+    public Book fromJpaToDomain(JpaBookDTO jpaBookDTO) {
+        return null;
+    }
+
+    @Override
+    public Book fromMongoToDomain(MongoBookDTO mongoBookDTO) {
+        return null;
+    }
+
+    @Override
+    public JpaBookDTO fromDomainToJpa(Book book) {
+        return null;
+    }
+
+    @Override
+    public MongoBookDTO fromDomainToMongo(Book book) {
+        return null;
+    }
 }
