2c357f09e8a785b39a9bfae5e568c1e9a6837afc
André L Reis <gato5andre26@gmail.com>
2024-10-27 21:06:36 +0000

    feat: lending + jpalending + fine + jpafine

---
+++ lms/src/main/java/pt/psoft/g1/psoftg1/lendingmanagement/dbSchema/JpaFineModel.java
@@ -0,0 +1,44 @@
+package pt.psoft.g1.psoftg1.lendingmanagement.dbSchema;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+public class JpaFineModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long pk;
+
+    @PositiveOrZero
+    @Column(updatable = false)
+    private int fineValuePerDayInCents;
+
+    /**Fine value in Euro cents*/
+    @PositiveOrZero
+    int centsValue;
+
+    @Setter
+    @OneToOne(optional = false, orphanRemoval = true)
+    @JoinColumn(name = "lending_pk", nullable = false, unique = true)
+    private JpaLendingModel lending;
+
+    /**
+     * Constructs a new {@code Fine} object. Sets the current value of the fine,
+     * as well as the fine value per day at the time of creation.
+     * @param   lending transaction which generates this fine.
+     * */
+    public JpaFineModel(JpaLendingModel lending, int centsValue) {
+        this.fineValuePerDayInCents = lending.getFineValuePerDayInCents();
+        this.centsValue = centsValue;
+        this.lending = Objects.requireNonNull(lending);
+    }
+
+    /**Protected empty constructor for ORM only.*/
+    protected JpaFineModel() {}
+}
--- lms/src/main/java/pt/psoft/g1/psoftg1/lendingmanagement/dbSchema/JpaLendingModel.java
+++ lms/src/main/java/pt/psoft/g1/psoftg1/lendingmanagement/dbSchema/JpaLendingModel.java
@@ -1,4 +1,119 @@
 package pt.psoft.g1.psoftg1.lendingmanagement.dbSchema;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import pt.psoft.g1.psoftg1.bookmanagement.dbSchema.JpaBookModel;
+import pt.psoft.g1.psoftg1.readermanagement.dbSchema.JpaReaderDetailsModel;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames={"LENDING_NUMBER"})})
 public class JpaLendingModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long pk;
+
+    @Column(name = "LENDING_NUMBER", length = 32)
+    @NotNull
+    @NotBlank
+    @Size(min = 6, max = 32)
+    private String lendingNumber;
+
+    @NotNull
+    @ManyToOne(fetch=FetchType.EAGER, optional = false)
+    private JpaBookModel book;
+
+
+    /**
+     * {@code Reader} associated with this {@code Lending}.
+     **/
+    @NotNull
+    @Getter
+    @ManyToOne(fetch=FetchType.EAGER, optional = false)
+    private JpaReaderDetailsModel readerDetails;
+
+    /**
+     * Date of this {@code Lending}'s creation.
+     * */
+    @NotNull
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.DATE)
+    @Getter
+    private LocalDate startDate;
+
+    /**
+     * Date this {@code Lending} is to be returned.
+     * */
+    @NotNull
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    @Getter
+    private LocalDate limitDate;
+
+    /**
+     * Date this {@code Lending} is actually returned. This field is initialized as {@code null}
+     * as the lending can never begin with the book already returned. The {@code null} value is used to
+     * check if a book has been returned.
+     * */
+    @Temporal(TemporalType.DATE)
+    @Getter
+    private LocalDate returnedDate;
+
+    // optimistic-lock
+    /**
+     * Version of the object, to handle concurrency of accesses.
+     * */
+    @Version
+    @Getter
+    private long version;
+
+    /**
+     * Optional commentary written by a reader when the book is returned.
+     * This field is initialized as null as the lending can never begin with the book already returned
+     * */
+    @Size(min = 0, max = 1024)
+    @Column(length = 1024)
+    private String commentary = null;
+
+    @Transient
+    private Integer daysUntilReturn;
+
+    @Transient
+    private Integer daysOverdue;
+
+    @Getter
+    private int fineValuePerDayInCents;
+
+
+    public JpaLendingModel(JpaBookModel book, JpaReaderDetailsModel readerDetails, String lendingNumber,
+                           int lendingDuration, int fineValuePerDayInCents, Integer daysUntilReturn, Integer daysOverdue) {
+        try {
+            this.book = Objects.requireNonNull(book);
+            this.readerDetails = Objects.requireNonNull(readerDetails);
+        }catch (NullPointerException e){
+            throw new IllegalArgumentException("Null objects passed to lending");
+        }
+        this.lendingNumber = lendingNumber;
+        this.startDate = LocalDate.now();
+        this.limitDate = LocalDate.now().plusDays(lendingDuration);
+        this.returnedDate = null;
+        this.fineValuePerDayInCents = fineValuePerDayInCents;
+        this.daysUntilReturn = daysUntilReturn;
+        this.daysOverdue = daysOverdue;
+    }
+
+    /**Protected empty constructor for ORM only.*/
+    protected JpaLendingModel() {}
+
 }
