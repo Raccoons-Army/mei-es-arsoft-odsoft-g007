1faeda8e98ed48970d5d50fe863cde0c7a8f3fed
André L Reis <gato5andre26@gmail.com>
2024-10-27 21:07:04 +0000

    feat: jpauser + jpareader

---
+++ lms/src/main/java/pt/psoft/g1/psoftg1/readermanagement/dbSchema/JpaReaderDetailsModel.java
@@ -0,0 +1,66 @@
+package pt.psoft.g1.psoftg1.readermanagement.dbSchema;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import pt.psoft.g1.psoftg1.genremanagement.dbSchema.JpaGenreModel;
+import pt.psoft.g1.psoftg1.shared.dbSchema.JpaEntityWithPhotoModel;
+import pt.psoft.g1.psoftg1.usermanagement.dbSchema.JpaReaderModel;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "READER_DETAILS")
+public class JpaReaderDetailsModel extends JpaEntityWithPhotoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long pk;
+
+    @OneToOne
+    private JpaReaderModel reader;
+
+    @Column(name = "READER_NUMBER")
+    private String readerNumber;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.DATE)
+    private LocalDate birthDate;
+
+    private String phoneNumber;
+
+    @Basic
+    private boolean gdprConsent;
+
+    @Basic
+    private boolean marketingConsent;
+
+    @Basic
+    private boolean thirdPartySharingConsent;
+
+    @Version
+    private Long version;
+
+    @ManyToMany
+    private List<JpaGenreModel> interestList;
+
+    public JpaReaderDetailsModel(String readerNumber, JpaReaderModel reader, LocalDate birthDate, String phoneNumber,
+                                 boolean gdpr, boolean marketing, boolean thirdParty, String photoURI, List<JpaGenreModel> interestList) {
+        setReader(reader);
+        setReaderNumber(readerNumber);
+        setPhoneNumber(phoneNumber);
+        setBirthDate(birthDate);
+        setGdprConsent(true);
+
+        setPhotoInternal(photoURI);
+        setMarketingConsent(marketing);
+        setThirdPartySharingConsent(thirdParty);
+        setInterestList(interestList);
+    }
+
+    protected JpaReaderDetailsModel() {
+        // for ORM only
+    }
+}
--- lms/src/main/java/pt/psoft/g1/psoftg1/usermanagement/dbSchema/JpaUserModel.java
+++ lms/src/main/java/pt/psoft/g1/psoftg1/usermanagement/dbSchema/JpaUserModel.java
@@ -1,4 +1,129 @@
 package pt.psoft.g1.psoftg1.usermanagement.dbSchema;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import pt.psoft.g1.psoftg1.usermanagement.model.Role;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "T_USER")
+@EntityListeners(AuditingEntityListener.class)
 public class JpaUserModel {
+
+    private static final long serialVersionUID = 1L;
+
+    // database primary key
+    @Id
+    @GeneratedValue
+    @Column(name="USER_ID")
+    private Long pk;
+
+    // optimistic lock concurrency control
+    @Version
+    private Long version;
+
+    // auditing info
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    // auditing info
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+
+    // auditing info
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private String createdBy;
+
+    // auditing info
+    @LastModifiedBy
+    @Column(nullable = false)
+    private String modifiedBy;
+
+    private boolean enabled = true;
+
+    @Column(unique = true, /*updatable = false,*/ nullable = false)
+    @Email
+    @NotNull
+    @NotBlank
+    private String username;
+
+    @Column(nullable = false)
+    @NotNull
+    @NotBlank
+    private String password;
+
+    private String name;
+
+    @ElementCollection
+    private final Set<Role> authorities = new HashSet<>();
+
+    protected JpaUserModel() {
+        // for ORM only
+    }
+
+    /**
+     *
+     * @param username
+     * @param password
+     */
+    public JpaUserModel(final String username, final String password) {
+        this.username = username;
+        setPassword(password);
+    }
+
+    /**
+     * factory method. since mapstruct does not handle protected/private setters
+     * neither more than one public constructor, we use these factory methods for
+     * helper creation scenarios
+     *
+     * @param username
+     * @param password
+     * @param name
+     * @return
+     */
+    public static JpaUserModel newUser(final String username, final String password, final String name) {
+        final var u = new JpaUserModel(username, password);
+        u.setName(name);
+        return u;
+    }
+
+    /**
+     * factory method. since mapstruct does not handle protected/private setters
+     * neither more than one public constructor, we use these factory methods for
+     * helper creation scenarios
+     *
+     * @param username
+     * @param password
+     * @param name
+     * @param role
+     * @return
+     */
+    public static JpaUserModel newUser(final String username, final String password, final String name, final String role) {
+        final var u = new JpaUserModel(username, password);
+        u.setName(name);
+        u.addAuthority(new Role(role));
+        return u;
+    }
+
+    public void addAuthority(final Role r) {
+        authorities.add(r);
+    }
+
 }
