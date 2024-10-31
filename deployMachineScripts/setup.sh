f126393b0b843efc6fa9229871006b9fd9ba1dc5
André L Reis <gato5andre26@gmail.com>
2024-10-31 19:18:35 +0000

    feat: script to setup deploy machine

--- deployMachineScripts/setup.sh
+++ deployMachineScripts/setup.sh
@@ -1,41 +1,45 @@
-# Update
+#!/bin/bash
+
+# Update package lists
 sudo apt update
 
-# Install command wget and unzip
-sudo apt install wget -y
-sudo apt install unzip -y
+# Install wget and unzip
+sudo apt install wget unzip -y
 
-# Install git
+# Install curl and git
 sudo apt install -y curl git
 
 # Install asdf
 echo "Installing asdf version manager..."
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
-# in case you use ~/.bashrc change the 2 lines below to ~/.bashrc
+
+# Add asdf to bash profile
 echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 source ~/.bashrc
 
-# asdf add java and maven plugins
+# Install java and maven plugins with asdf
 asdf plugin add java
 asdf plugin add maven
 
-# Install jdk 17 and maven 3.9
+# Install JDK 17 and Maven 3.9
 asdf install java openjdk-17
 asdf install maven 3.9.9
-# Set global versions
+
+# Set global versions for Java and Maven
 asdf global java openjdk-17
 asdf global maven 3.9.9
 
-# Install and boot h2 
-wget https://github.com/h2database/h2database/releases/download/version-2.3.232/h2-2024-08-11.zip
-unzip h2-2024-08-11.zip
-#java -jar h2/bin/h2*.jar -tcp -tcpAllowOthers -tcpPort 9092
+# Download and install H2 Database
+H2_VERSION="2.3.232"
+H2_ZIP_NAME="h2-2024-08-11"
+wget "https://github.com/h2database/h2database/releases/download/version-${H2_VERSION}/h2-${H2_ZIP_NAME}.zip"
+unzip "h2-${H2_ZIP_NAME}.zip"
+rm "h2-${H2_ZIP_NAME}.zip"  # Clean up the zip file after extraction
 
-# add h2 as a service
-# Variables
-USERNAME="root"  # Replace with your actual username
-H2_JAR_PATH="/root/h2/bin/h2-2.3.232.jar"  # Replace with the actual path to H2 jar
+# Variables for H2 service
+USERNAME="root"  # Replace with your actual username if different
+H2_JAR_PATH="/root/h2/bin/h2-${H2_VERSION}.jar"  # Adjust to match extracted jar file name
 SERVICE_FILE="/etc/systemd/system/h2.service"
 
 # Create the H2 service file
@@ -47,7 +51,7 @@ After=network.target
 [Service]
 User=${USERNAME}
 ExecStartPre=/bin/bash -c 'fuser -k 9092/tcp || true'  # Kill any process using port 9092
-ExecStart=/${USERNAME}/.asdf/shims/java -jar ${H2_JAR_PATH} -tcp -tcpAllowOthers -tcpPort 9092
+ExecStart=/root/.asdf/shims/java -jar ${H2_JAR_PATH} -tcp -tcpAllowOthers -tcpPort 9092
 SuccessExitStatus=143
 Restart=on-failure
 
@@ -62,4 +66,6 @@ sudo systemctl daemon-reload
 sudo systemctl enable h2
 sudo systemctl start h2
 
+# Check H2 service status
+sudo systemctl status h2
 
