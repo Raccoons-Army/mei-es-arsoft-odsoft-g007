300712c1e65e012a7d25eb249bcd77fbb784a808
KirinDev <111713739+KirinDev@users.noreply.github.com>
2024-11-01 22:09:18 +0000

    feat: implemented some white-box unit tests on book model

--- lms/src/test/java/pt/psoft/g1/psoftg1/bookmanagement/model/BookWBTest.java
+++ lms/src/test/java/pt/psoft/g1/psoftg1/bookmanagement/model/BookWBTest.java
@@ -1,4 +1,54 @@
 package pt.psoft.g1.psoftg1.bookmanagement.model;
 
+import org.junit.jupiter.api.Test;
+import pt.psoft.g1.psoftg1.authormanagement.model.Author;
+import pt.psoft.g1.psoftg1.authormanagement.model.FactoryAuthor;
+import pt.psoft.g1.psoftg1.genremanagement.model.FactoryGenre;
+
+import static org.mockito.Mockito.*;
+
 public class BookWBTest {
+
+    private final String validIsbn = "9782826012092";
+    private final String validTitle = "Encantos de contar";
+
+    @Test
+    void verifyAddAuthorIsCalledOneTime() throws InstantiationException {
+        Author authorDouble = mock(Author.class);
+
+        FactoryGenre factoryGenreDouble = mock(FactoryGenre.class);
+        FactoryAuthor factoryAuthorDouble = mock(FactoryAuthor.class);
+
+        when(factoryAuthorDouble.newAuthor("aaa1", "João Alberto", "O João Alberto nasceu em Chaves e foi pedreiro a maior parte da sua vida.", null))
+                .thenReturn(authorDouble);
+
+        Book book = new Book(validIsbn, validTitle, null, null, factoryGenreDouble, factoryAuthorDouble);
+        book.addAuthor("aaa1", "João Alberto", "O João Alberto nasceu em Chaves e foi pedreiro a maior parte da sua vida.", null);
+
+        // verify
+        verify(factoryAuthorDouble, times(1)).newAuthor("aaa1", "João Alberto", "O João Alberto nasceu em Chaves e foi pedreiro a maior parte da sua vida.", null);
+
+    }
+
+    @Test
+    void verifyAddAuthorIsCalledNTimes() throws InstantiationException {
+        final int n = 10;
+
+        Author authorDouble = mock(Author.class);
+
+        FactoryGenre factoryGenreDouble = mock(FactoryGenre.class);
+        FactoryAuthor factoryAuthorDouble = mock(FactoryAuthor.class);
+
+        when(factoryAuthorDouble.newAuthor("aaa1", "João Alberto", "O João Alberto nasceu em Chaves e foi pedreiro a maior parte da sua vida.", null))
+                .thenReturn(authorDouble);
+
+        Book book = new Book(validIsbn, validTitle, null, null, factoryGenreDouble, factoryAuthorDouble);
+
+        for (int i = 0; i< n; i++) {
+            book.addAuthor("aaa1", "João Alberto", "O João Alberto nasceu em Chaves e foi pedreiro a maior parte da sua vida.", null);
+        }
+
+        // verify
+        verify(factoryAuthorDouble, times(n)).newAuthor("aaa1", "João Alberto", "O João Alberto nasceu em Chaves e foi pedreiro a maior parte da sua vida.", null);
+    }
 }
